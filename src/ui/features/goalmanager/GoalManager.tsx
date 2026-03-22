62dfc7c6ec8feda1326c6dd9e98f877be4d07853
TshifhiwaT <thekhwetshifhiwa@gmail.com>
2026-03-22 13:43:57 +0200

    Add PUT request to lib

--- src/ui/features/goalmanager/GoalManager.tsx
+++ src/ui/features/goalmanager/GoalManager.tsx
@@ -96,7 +96,7 @@ export function GoalManager(props: Props) {
 
     dispatch(updateGoalRedux(updatedGoal))
 
-    // TODO(TASK-3) Update database
+    updateGoalApi(props.goal.id, updatedGoal)
   }
 
   return (
